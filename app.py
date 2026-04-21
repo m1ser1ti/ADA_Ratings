b661f5b5d346b61d27d07f26eadfdf4d3e2a885c
Aldiyar Temirbek <143316533+m1ser1ti@users.noreply.github.com>
2026-04-22 02:29:30 +0400

    Updated app.py with new content.

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import os
 app = Flask(__name__, static_folder='public', static_url_path='')
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ada_ratings.db'
+db_url = os.environ.get("DATABASE_URL", "sqlite:///ada_ratings.db")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -46,7 +49,6 @@ class Review(db.Model):
 # Создаём таблицы и заполняем базу при первом запуске
 with app.app_context():
     db.create_all()
-
 
 
 @app.route('/')
